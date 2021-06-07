706dbc1e98dab53a32cfea99e080b4af40592ec6
t-willi-28 <taku.0628william@gmail.com>
2021-06-07 17:16:46 +0900

    ユーザー登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :logs
+  has_many :comments
+
+  validates :nickname, presence: true
 end
