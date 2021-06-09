08f1c37c95ca50a720b558012fb15a746b4f14dd
t-willi-28 <taku.0628william@gmail.com>
2021-06-09 14:48:01 +0900

    投稿機能の実装

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,4 +2,22 @@ class LogsController < ApplicationController
   def index
     @log = Log.all
   end
+
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+    if @log.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def log_params
+    params.require(:log).permit(:place, :description, :point, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,11 @@
 class Log < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :place
+    validates :description
+    validates :point
+    validates :image
+  end
 end
