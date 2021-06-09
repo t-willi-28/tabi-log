f73bbb31b7ebe0a7abd585a6706e7e9f16defef1
t-willi-28 <taku.0628william@gmail.com>
2021-06-09 18:03:28 +0900

    詳細表示と削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'logs#index'
-  resources :logs, only: [:index, :new, :create]
+  resources :logs, only: [:index, :new, :create, :show, :destroy]
 end
