4ee5dce2300a9f51606b9e1b39a49725bfcc5774
t-willi-28 <taku.0628william@gmail.com>
2021-06-10 19:23:53 +0900

    修正

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,5 @@
 class LogsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:destroy]
 
   def index
     @log = Log.all
@@ -36,7 +35,4 @@ class LogsController < ApplicationController
     params.require(:log).permit(:place, :description, :point, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to root_path if current_user.id != @log.user_id
-  end
 end
